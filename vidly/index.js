7453f2334bdce2aba5fbfe01010a320a437e32d7
Remigio <rvildacci@gmail.com>
2019-01-28 23:50:14 +0100

    Adding Rentals model and two phase commit on Mongo DB

--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 const { expressPort } = require('../config');
@@ -15,6 +16,8 @@ mongoose.connect(`mongodb://${mongoDBhost}:${mongoDBport}/vidly`, {useNewUrlPars
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 app.listen(expressPort, () =>{
   console.log(`Vidly run and listening on ${expressPort}..`);
