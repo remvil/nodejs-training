5d22b0a793d8e7a0bfffd8562612b9b2cbc95440
Remigio <rvildacci@gmail.com>
2019-01-30 00:36:19 +0100

    auth

--- vidly/index.js
+++ vidly/index.js
@@ -1,8 +1,12 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+
 const express = require('express');
 const app = express();
 const { expressPort } = require('../config');
@@ -18,6 +22,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 app.listen(expressPort, () =>{
   console.log(`Vidly run and listening on ${expressPort}..`);
